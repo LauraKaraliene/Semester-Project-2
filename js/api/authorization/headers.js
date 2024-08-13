5de9c21a704d8f779bd6e69679b600b11e1cb2f1
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-08-13 20:47:14 +0200

    adds headers funtion

---
+++ js/api/authorization/headers.js
@@ -0,0 +1,22 @@
+import { get } from "../../utils/storage/get.js";
+
+// maybe use this instead?
+// import { getToken } from "../../utils/helpers/getToken.js";
+import { apiKey } from "./apiKey.js";
+
+export function headers(hasBody = false) {
+  const headers = new Headers();
+  const token = get("token");
+
+  if (token) {
+    headers.append("Authorization", `Bearer ${token}`);
+  }
+  if (apiKey) {
+    headers.append("X-Noroff-API-Key", apiKey);
+  }
+  if (hasBody) {
+    headers.append("Content-Type", "application/json");
+  }
+
+  return headers;
+}
